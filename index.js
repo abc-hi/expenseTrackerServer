6eb3309be0987cbcc8b78b22eb695ad25d5659cf
abc-hi <revathimohancse@gmail.com>
2024-08-18 18:00:04 -0700

    small change done

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Expense Tracker API');
+});
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
